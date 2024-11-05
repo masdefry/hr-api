2321f8e592a0b8990d088077194d1f040f9bc8b4
masdefry <muhammaddefryan@gmail.com>
2024-11-05 14:54:02 +0700

    fix:error

--- dist/services/auth.service/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Role = void 0;
-var Role;
-(function (Role) {
-    Role[Role["HR"] = 0] = "HR";
-    Role[Role["MANAGER"] = 1] = "MANAGER";
-    Role[Role["STAFF"] = 2] = "STAFF";
-})(Role || (exports.Role = Role = {}));
